364dfa43eb5a616314b9c84be9dd5dfc93634e96
EL Putnam <elputnam@inaction.ie>
2023-07-04 13:49:27 +0100

    add photo and change pixels to glitter

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 //The law of table in a quantum field: a deliberate misinterpretation
 
 let pixels = [];
+let img;
+function preload (){
+ img = loadImage('assets/ELDad_Cigar.jpg');
+}
 
 function setup() {
+  // createCanvas(img.width*.34, img.height*.34, WEBGL);
   createCanvas(windowWidth, windowHeight, WEBGL);
   colorMode(HSB, 360, 100, 100, 100);
   frameRate(20);
-  for (var j = 0; j < 500; j++){
-    pixels[j] = new Pixel(random(-width/2, width/2), random(-height/2, height/2), 10)
+  rectMode(CENTER);
+  for (var j = 0; j < 2000; j++){
+    pixels[j] = new Pixel(random(-width/2, width/2), random(-height/2, height/2), 3)
   }
 }
 
@@ -15,30 +21,47 @@ function draw() {
   // background(0);
   // fill(260, random(100), 100);
   // square(random(-width/2, width/2), random(-height/2, height/2), 10);
-  //pixels
-  push();
-  translate(0,0,-500);
-  scale(1.5)
-  if (frameCount%2==0){
-    
-  for (let i = 0; i < pixels.length; i++) {
-    pixels[i].move();
-		pixels[i].show();
+  if (frameCount%int(random(2))==0){
+    //image
+    push();
+    // translate(-width*1.25, -height*1.25, -600);
+    // translate(-width, -height, -600);
+    imageMode(CENTER);
+    translate(0, 0, -300)
+    tint(random(255), random(100));
+    scale(2);
+    image(img, 0, 0, img.width/4, img.height/4);
+    pop();
   }
+
+ 
+  if (frameCount%int(random(5))==0){
+    //pixels/glitter
+    push();
+    translate(0,0,-400);
+    scale(1.75)
+    for (let i = 0; i < pixels.length; i++) {
+      pixels[i].move();
+		  pixels[i].show();
+    }
+   pop();
 }
-  if (frameCount%3==0){
+
+ 
+  if (frameCount%4==0){
     background(0);
   }
-pop();
+  
 
 //tabletop
+
   stroke(50);
   if (frameCount%10==0){
   fill(80, random(100), random(100), random(100));
   }
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  rotateZ(frameCount * 0.01);
+  //rotateZ(frameCount * 0.01);
   box(300, 500, 20);
   //leg 1
   push();
@@ -77,8 +100,6 @@ pop();
   cone(10, 400);
   pop();
 
- 
-
 }
 
 class Pixel{
@@ -94,7 +115,9 @@ class Pixel{
   }
   show() {
     noStroke();
-    fill(random(255));
-    rect(this.x, this.y, random(this.r), random(this.r));
+    fill(50, random(100), random(100));
+    // fill(random(255));
+    // rect(this.x, this.y, random(this.r), random(this.r));
+    circle(this.x, this.y, random(this.r));
   }
 }
