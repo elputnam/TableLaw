2390319c20853b734f5504877359908de7e16aea
EL Putnam <elputnam@inaction.ie>
2023-07-04 19:05:22 +0100

    double image, change animation

--- sketch.js
+++ sketch.js
@@ -1,45 +1,46 @@
 //The law of table in a quantum field: a deliberate misinterpretation
 
 let pixels = [];
-let img;
+let img1;
+let img2;
 function preload (){
- img = loadImage('assets/ELDad_Cigar.jpg');
+ img1 = loadImage('assets/ELDad_Cigar_2.jpg');
+ img2 =loadImage('assets/ELDad_Cigar.jpg')
 }
 
 function setup() {
   // createCanvas(img.width*.34, img.height*.34, WEBGL);
   createCanvas(windowWidth, windowHeight, WEBGL);
   colorMode(HSB, 360, 100, 100, 100);
-  frameRate(20);
+  frameRate(15);
   rectMode(CENTER);
   for (var j = 0; j < 2000; j++){
-    pixels[j] = new Pixel(random(-width/2, width/2), random(-height/2, height/2), 5)
+    pixels[j] = new Pixel(random(-width/2, width/2), random(-height/2, height/2), 7)
   }
 }
 
 function draw() {
-  // background(0);
-  // fill(260, random(100), 100);
-  // square(random(-width/2, width/2), random(-height/2, height/2), 10);
-  if (frameCount%int(random(2))==0){
+  // if (frameCount%2==0){
     //image
     push();
-    // translate(-width*1.25, -height*1.25, -600);
-    // translate(-width, -height, -600);
     imageMode(CENTER);
-    translate(0, 0, -300)
-    tint(random(255), random(100));
+    translate(0, 0, -500)
+    
     scale(2);
-    image(img, 0, 0, img.width/4, img.height/4);
+    // img.filter(INVERT);
+    tint(random(255));
+    image(img2, 0, 0, img2.width/4, img2.height/4);
+    tint(random(255), random(100));
+    image(img1, 0, 0, img1.width/4, img1.height/4);
     pop();
-  }
+  // }
 
  
-  if (frameCount%int(random(5))==0){
+ if (frameCount%int(random(5))==0){
     //pixels/glitter
     push();
     translate(0,0,-400);
-    scale(1.75)
+    scale(1.75);
     for (let i = 0; i < pixels.length; i++) {
       pixels[i].move();
 		  pixels[i].show();
@@ -49,16 +50,44 @@ function draw() {
 }
 
  
-  if (frameCount%4==0){
+  if (frameCount%5==0){
     background(0);
   }
   
 
 //tabletop
+table();
+ 
+}
 
+class Pixel{
+  constructor(x, y, r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+
+  edges(){
+    if (this.y > height/2)
+    this.y = -height/2 
+}
+  move() {
+    //this.x = random(-width/2, width/2);
+    this.y += random(10);
+  }
+  show() {
+    noStroke();
+    // fill(random(250, 360), random(100), random(100), random(100));
+    fill(random(255));
+    // rect(this.x, this.y, random(this.r), random(this.r));
+    rect(this.x, this.y, random(this.r), random(this.r));
+  }
+}
+
+function table(){
   stroke(50);
   if (frameCount%10==0){
-  fill(80, random(100), random(100), random(100));
+  fill(random(80, 180), random(100), random(100), random(100));
   }
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
@@ -101,28 +130,4 @@ function draw() {
   cone(10, 400);
   pop();
 
-}
-
-class Pixel{
-  constructor(x, y, r){
-    this.x = x;
-    this.y = y;
-    this.r = r;
-  }
-
-  edges(){
-    if (this.y > height/2)
-    this.y = -height/2 
-}
-  move() {
-    //this.x = random(-width/2, width/2);
-    this.y += 10;
-  }
-  show() {
-    noStroke();
-    fill(320, random(100), random(100));
-    // fill(random(255));
-    // rect(this.x, this.y, random(this.r), random(this.r));
-    circle(this.x, this.y, random(this.r));
-  }
 }
